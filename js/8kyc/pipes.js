e352af3585c7e1463a67c2d232693e3cd4163c62
ngit <googladmen@gmail.com>
2024-07-24 06:25:30 +0300

    js day 2

---
+++ js/8kyc/pipes.js
@@ -0,0 +1,10 @@
+function pipeFix(numbers){
+    let start = numbers[0];
+    let finish = numbers[numbers.length-1];
+    let ara = [];
+    while(start <= finish){  
+      ara.push(start);
+      start+=1;}
+      
+    return ara;
+    }
